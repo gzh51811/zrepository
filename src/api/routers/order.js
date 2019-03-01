4d57c0cce8cad347d6b534a59e6da57f51add900
zhang <1453512279@qq.com>
2019-03-01 17:49:16 +0800

    差不多完成了

--- src/api/routers/order.js
+++ src/api/routers/order.js
@@ -8,10 +8,21 @@ const ObjectId = require('mongodb').ObjectId
 const router = new Router();
 
 router.post('/', async (ctx, next) => {
-    let {_id} = ctx.request.body;
-    let res = await db.delete('orderlist', {_id: ObjectId(_id)});
+    try {
+        var {method} = ctx.request.body;
+        if(method == 'update') {
+            var {_id, orderstate} = ctx.request.body;
+            var res = await db.update('orderlist', {_id: ObjectId(_id)}, {$set: {orderstate: orderstate}});
 
-    ctx.body = res;
+            ctx.body = res;
+        }
+    } catch(err) {
+        // console.log(err);
+        let {_id} = ctx.request.body;
+        let res = await db.delete('orderlist', {_id: ObjectId(_id)});
+    
+        ctx.body = res;
+    } 
 });
 
 router.get('/', async (ctx, next) => {
